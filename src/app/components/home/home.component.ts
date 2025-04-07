24a5a9063a16823b0e37921a4cf7825b317b6559
Ja1zinh0 <joao2011bha@hotmail.com>
2025-04-07 18:59:06 -0300

    changed home html organization

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { HeaderComponent } from '../../shared/header/header.component';
+import { LeftsideComponent } from '../leftside/leftside.component';
+import { RightsideComponent } from '../rightside/rightside.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, LeftsideComponent, RightsideComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   schemas: [],
