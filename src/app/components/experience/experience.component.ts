44b847dacaa1698bed237e7be11365066d356516
Ja1zinh0 <joao2011bha@hotmail.com>
2025-04-15 07:40:14 -0300

    added experience card component

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -1,11 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
+interface Experience {
+  company: string;
+  title: string;
+  duration: string;
+  description: string;
+  expanded: boolean;
+}
 
 @Component({
   selector: 'app-experience',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './experience.component.html',
-  styleUrl: './experience.component.scss'
+  styleUrl: './experience.component.scss',
 })
 export class ExperienceComponent {
+  experiences: Experience[] = [
+    {
+      company: 'Inov.ai Tech',
+      title: 'Desenvolvedor Fullstack',
+      duration: '06/2024 - 02/2025',
+      description:
+        'Participei de automações com IA, criei pipelines com Python, Node.js e Kubernetes.',
+      expanded: false,
+    },
+    {
+      company: 'Fatec Ribeirão',
+      title: 'Estagiário em Suporte',
+      duration: '01/2023 - 05/2024',
+      description:
+        'Auxiliei na manutenção de sistemas e automatização de relatórios.',
+      expanded: false,
+    },
+  ];
 
+  toggleDetails(index: number): void {
+    this.experiences[index].expanded = !this.experiences[index].expanded;
+  }
 }
--- src/app/components/rightside/rightside.component.ts
+++ src/app/components/rightside/rightside.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { AboutComponent } from '../about/about.component';
+import { ExperienceComponent } from '../experience/experience.component';
 
 @Component({
   selector: 'app-rightside',
-  imports: [AboutComponent],
+  imports: [AboutComponent, ExperienceComponent],
   templateUrl: './rightside.component.html',
   styleUrl: './rightside.component.scss',
 })
