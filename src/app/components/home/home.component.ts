578109da3c21550c011f8bac36a4e99019ae3e1d
Ja1zinh0 <joao2011bha@hotmail.com>
2025-04-23 08:33:53 -0300

    refactor: changing project to use tailwind

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { LeftsideComponent } from '../leftside/leftside.component';
 import { RightsideComponent } from '../rightside/rightside.component';
-import { HeaderComponent } from '../header/header.component';
+import { MiddleContentComponent } from '../middle-content/middle-content.component';
 
 @Component({
   selector: 'app-home',
-  imports: [LeftsideComponent, RightsideComponent, HeaderComponent],
+  imports: [LeftsideComponent, RightsideComponent, MiddleContentComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   schemas: [],
---
+++ src/app/components/middle-content/middle-content.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-middle-content',
+  imports: [],
+  templateUrl: './middle-content.component.html',
+  styleUrl: './middle-content.component.scss'
+})
+export class MiddleContentComponent {
+
+}
