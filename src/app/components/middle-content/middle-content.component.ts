ba57e18c78bf6c9252d9aab2b26c9069062b289b
ja1zinh0 <joao2011bha@hotmail.com>
2025-05-01 17:57:16 -0300

    added about text

--- src/app/components/middle-content/middle-content.component.ts
+++ src/app/components/middle-content/middle-content.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { AboutComponent } from '../about/about.component';
 
 @Component({
   selector: 'app-middle-content',
-  imports: [],
+  imports: [AboutComponent],
   templateUrl: './middle-content.component.html',
-  styleUrl: './middle-content.component.scss'
+  styleUrl: './middle-content.component.scss',
 })
-export class MiddleContentComponent {
-
-}
+export class MiddleContentComponent {}
