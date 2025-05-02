0033a91b032a4e5edf9f8a8a5e693694ce54eaec
ja1zinh0 <joao2011bha@hotmail.com>
2025-05-02 08:06:51 -0300

    changed card style and structure

--- src/app/components/experience/experience-card/experience-card.component.ts
+++ src/app/components/experience/experience-card/experience-card.component.ts
@@ -1,11 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-experience-card',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './experience-card.component.html',
-  styleUrl: './experience-card.component.scss'
+  styleUrl: './experience-card.component.scss',
 })
 export class ExperienceCardComponent {
+  cards = [
+    {
+      title: 'Experiência A',
+      time: '24/02/2024',
+      company: 'Empresa1',
+      content: 'Fiz tal coisa bla bla bla 1',
+      isOpen: false,
+    },
+    {
+      title: 'Experiência B',
+      time: '24/02/2024',
+      company: 'empresa2',
+      content: 'Fiz tal coisa bla bla bla 2',
+      isOpen: false,
+    },
+  ];
 
+  toggleCard(index: number) {
+    this.cards[index].isOpen = !this.cards[index].isOpen;
+  }
 }
