5eeea293d99e93ac959001b559544958c293e586
ja1zinh0 <joao2011bha@hotmail.com>
2025-05-14 18:13:17 -0300

    added english cv

--- src/app/components/curriculum/curriculum.component.ts
+++ src/app/components/curriculum/curriculum.component.ts
@@ -1,22 +1,43 @@
-import { Component } from '@angular/core';
-import {
-  NgxExtendedPdfViewerModule,
-  NgxExtendedPdfViewerService,
-  pdfDefaultOptions,
-} from 'ngx-extended-pdf-viewer';
+import { Component, OnInit } from '@angular/core';
+import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
+import { LanguageService } from '../../services/language.service';
 
 @Component({
   selector: 'app-curriculum',
+  standalone: true,
   imports: [NgxExtendedPdfViewerModule],
-  providers: [NgxExtendedPdfViewerService],
   templateUrl: './curriculum.component.html',
   styleUrl: './curriculum.component.scss',
 })
-export class CurriculumComponent {
+export class CurriculumComponent implements OnInit {
+  pdfSrc: string = '';
+  currentLanguage: 'en' | 'pt' = 'pt';
+
+  constructor(private languageService: LanguageService) {}
+
+  ngOnInit() {
+    // Inicializa o PDF conforme o idioma atual
+    this.currentLanguage = this.languageService.getLanguage();
+    this.updatePdfSrc(this.currentLanguage);
+
+    // Reage às mudanças de idioma
+    this.languageService.getLanguageObservable().subscribe((lang) => {
+      this.currentLanguage = lang;
+      this.updatePdfSrc(lang);
+    });
+  }
+
+  updatePdfSrc(lang: 'en' | 'pt') {
+    this.pdfSrc =
+      lang === 'en'
+        ? '/assets/joao-henrique-en.pdf'
+        : '/assets/joao-henrique.pdf';
+  }
+
   downloadPDF() {
     const link = document.createElement('a');
-    link.href = '/assets/joao-henrique.pdf';
-    link.download = 'joao-henrique.pdf';
+    link.href = this.pdfSrc;
+    link.download = this.pdfSrc.split('/').pop()!;
     link.click();
   }
 }
